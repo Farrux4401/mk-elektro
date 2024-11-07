2cfcac487e6f14d7b71ccabb47673b83d2cde94d
Farrux4401 <leganesalaves@gmail.com>
2024-11-07 16:17:15 +0500

    last changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,33 @@
 import { defineConfig } from 'vite';
 
+
+
+import path from 'path';
+
 export default defineConfig({
-  root: 'src',
+  root: 'src', // Set project root to `src` if your entry HTML is in this folder
   build: {
-    outDir: '../dist',
+    outDir: '../dist', // Relative to the root, which is 'src', this will output to "dist" in the main directory
+    emptyOutDir: true, // Ensures the output directory is emptied before each build
   },
   optimizeDeps: {
     include: ['jquery'], // Ensures jQuery is bundled and available
   },
-
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Optional, if you want to use aliases
+    },
+  },
 });
+
+
+// export default defineConfig({
+//   root: 'src',
+//   build: {
+//     outDir: '../dist',
+//   },
+//   optimizeDeps: {
+//     include: ['jquery'], // Ensures jQuery is bundled and available
+//   },
+
+// });
